df07b4a3a20d09ffe4b6e9c9cae24d6ddfc65b35
YoNyeoSeok <yys8646@postech.ac.kr>
2018-08-08 14:00:46 +0900

    batch 10 1 modify1

--- exper_batch_10_1.sh
+++ exper_batch_10_1.sh
@@ -7,6 +7,6 @@ python main.py --learning_rate 1e-5 --log_folder 1e-5_batch_10_1_after100000 --l
 for i in 2 3 4 5 6 7 8 9 10
 do
 	mkdir logs/1e-5_batch_10_1_after${i}00000	
-	python main.py --learning_rate 1e-5 --log_folder 1e-5_batch_10_1_after${i}00000 --load_model logs/1e-5_batch_10_1_after$((${i-1}-1))00000/99999.pt --max_iter 99999
+	python main.py --learning_rate 1e-5 --log_folder 1e-5_batch_10_1_after${i}00000 --load_model logs/1e-5_batch_10_1_after$((${i}-1))00000/99999.pt --max_iter 99999
 done
 
