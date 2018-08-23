f87c05af469369faef266280ba79cd48812fb31e
YoNyeoSeok <yys8646@postech.ac.kr>
2018-08-23 19:05:06 +0900

    exper add

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
+lr="1e-4"
+
 echo "${BASH_VERSION}"
 for i in {1..4}
 do
-    echo $i
+    python test_main.py --learning_rate $lr
+    echo $i$lr
 done
--- test_exper.sh
+++ test_exper.sh
@@ -1,11 +1,13 @@
-mkdir logs/test_1e-5_before100000
-python test_main.py --learning_rate 1e-5 --log_folder test_1e-5_before100000 --max_iter 99999
+lr="1e-4"
 
-mkdir logs/test_1e-5_after100000	
-python test_main.py --learning_rate 1e-5 --log_folder test_1e-5_after100000 --load_model logs/test_1e-5_before100000/99999.pt --max_iter 99999
+mkdir logs/test_${lr}_before100000
+python test_main.py --learning_rate ${lr} --log_folder test_${lr}_before100000 --max_iter 99999
+
+mkdir logs/test_${lr}_after100000	
+python test_main.py --learning_rate ${lr} --log_folder test_${lr}_after100000 --load_model logs/test_${lr}_before100000/99999.pt --max_iter 99999
 
 for i in {2..49}
 do
-	mkdir logs/test_1e-5__after${i}00000	
-    python test_main.py --learning_rate 1e-5 --log_folder test_1e-5_after${i}00000 --load_model logs/test_1e-5_after$((${i}-1))00000/99999.pt --max_iter 99999
+	mkdir logs/test_${lr}_after${i}00000	
+    python test_main.py --learning_rate ${lr} --log_folder test_${lr}_after${i}00000 --load_model logs/test_${lr}_after$((${i}-1))00000/99999.pt --max_iter 99999
 done
