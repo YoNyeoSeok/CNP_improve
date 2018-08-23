730fa2e8c888d888e7a70cc2b4669c4e74344b35
YoNyeoSeok <yys8646@postech.ac.kr>
2018-08-23 18:54:11 +0900

    test_exper main to test_main

--- test_exper.sh
+++ test_exper.sh
@@ -1,11 +1,11 @@
-mkdir logs/test_before100000
-python main.py --log_folder test_before100000 --max_iter 99999
+mkdir logs/test_1e-5_before100000
+python test_main.py --learning_rate 1e-5 --log_folder test_1e-5_before100000 --max_iter 99999
 
-mkdir logs/test_after100000	
-python main.py --log_folder test_after100000 --load_model logs/test_before100000/99999.pt --max_iter 99999
+mkdir logs/test_1e-5_after100000	
+python test_main.py --learning_rate 1e-5 --log_folder test_1e-5_after100000 --load_model logs/test_1e-5_before100000/99999.pt --max_iter 99999
 
 for i in {2..49}
 do
-	mkdir logs/test_after${i}00000	
-    python main.py --log_folder test_after${i}00000 --load_model logs/test_after$((${i}-1))00000/99999.pt --max_iter 99999
+	mkdir logs/test_1e-5__after${i}00000	
+    python test_main.py --learning_rate 1e-5 --log_folder test_1e-5_after${i}00000 --load_model logs/test_1e-5_after$((${i}-1))00000/99999.pt --max_iter 99999
 done
