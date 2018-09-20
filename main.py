51777d1b5b7f68210da7d2e562f686b8238bbf68
YoNyeoSeok <yys8646@postech.ac.kr>
2018-09-20 19:54:03 +0900

    complete 1v

--- main.py
+++ main.py
@@ -2,36 +2,50 @@
 python main.py 
 """
 
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
 import numpy as np
 import torch
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, WhiteKernel
 import os
 
 from model import CNP_Net
+from data_generator import DataGenerator
 
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--func_limit", metavar='f', type=int, default=0,
-        help='train function number, 0 is infinite (default=0)')
-parser.add_argument("--max_iter", type=int, default=10000,
+parser.add_argument("--task_limit", type=int, default=0,
+        help='number of task to train, 0 is infinite (default=0)')
+parser.add_argument("--max_epoch", type=int, default=10000,
         help='max iteration (default=10000')
-parser.add_argument("--learning_rate", metavar="lr", type=float, default=1e-4,
+parser.add_argument("--interval", type=int, default=1000,
+        help='max iteration (default=10000')
+parser.add_argument("-lr", "--learning_rate", type=float, default=1e-4,
         help="learning rate (default=1e-4)")
-parser.add_argument("--batch_size", type=int, nargs=2, default=[1,1],
-        help='function batch size, observe data batch size (default: [1,1])')
+parser.add_argument("-bs", "--batch_size", type=int, nargs='?', default=32,
+        help='batch size (default: 32)')
+parser.add_argument("-ns", "--num_samples", type=int, nargs=2, default=[5, 5],
+        help='number of samples, few-shot number of train, test (default: [5, 5])')
+parser.add_argument("-nsr", "--num_samples_range", metavar=('min', 'max'), type=int, nargs=2, default=[1, 51],
+        help='number of samples random range, few-shot number random range (default: [1 51])')
+parser.add_argument("-rs", "--random_sample", action='store_true',
+        help='number of samples is random flag')
+parser.add_argument("-nnd", "--not_disjoint_data", action='store_false',
+        help='train test set data are not disjoint setting flag')
+parser.add_argument("--input_range", metavar=('min', 'max'), type=int, nargs=2, default=[-2, 2],
+        help='function input range (default: [-2, 2])')
+parser.add_argument("--log", action='store_true',
+        help="save loss, fig and model log")
 parser.add_argument("--log_folder", type=str, default="log",
         help="log folder name in logs/ (default: log)")
-parser.add_argument("--input_range", metavar='R', type=int, nargs=2, 
-        default=[-2, 2], help="input range (default: [-2, 2])")
-parser.add_argument("--fig_show", metavar='S', type=bool, default=False,
-        help="figure show during traing or not (default=False)")
+parser.add_argument("--datasource", type=str, nargs='?', default="gp1d", choices=["gp1d", "branin"],
+        help="gp1d or branin")
+parser.add_argument("--fig_show", action='store_true',
+        help="figure show during traing")
 parser.add_argument("--load_model", type=str,
         help="load model. format: folder/iteration")
 parser.add_argument("--model_layers", nargs='?', default=None,
-        help="model layers: default=None, means {'h':[8, 32, 128], 'g':[129, 64, 32, 16, 8]}")
+        help="model layers: default=None, means {'h':[8, 32, 128], 'g':[128, 64, 32, 16, 8]}")
 parser.add_argument("--precision", nargs='?', default=None,
         help="change variance to precision (inverse) option")
 args = parser.parse_args()
@@ -43,148 +57,184 @@ def plot_fig(fig, x, y_min, y_cov, color='k'):
             y_min.reshape(-1) + np.sqrt(np.diag(y_cov)),
             alpha=.5, color=color)
 
-
-
 def main():
-    x_min = -2
-    x_max = 2 
-    n_observation = 51
-    x_plot = np.linspace(x_min, x_max, n_observation).reshape(-1, 1)
+    data_generator = DataGenerator(datasource=args.datasource, 
+                                   batch_size=args.batch_size,
+                                   random_sample=args.random_sample,
+                                   disjoint_data=not args.not_disjoint_data,
+                                   num_samples=args.num_samples,
+                                   num_samples_range=args.num_samples_range,
+                                   input_range=args.input_range,
+                                   task_limit=args.task_limit)
+    
+    space_samples = data_generator.generate_space_sample()
+#    x_plot = np.linspace(args.input_range[0], args.input_range[1], args.num_samples[-1]).reshape(-1, 1)
+
+    # x_plot = np.linspace(x_min, x_max, n_observation).reshape(-1, 1)
 
     if args.load_model is not None:
         model = torch.load(args.load_model)
     elif args.model_layers is not None:
-        model = CNP_Net(io_dims=[1, 1], n_layers=[3, 5], layers_dim={'h':[128, 128, 128], 'g':[129, 128, 128, 128, 128]}).float()
+        model = CNP_Net(io_dims=data_generator.io_dims, 
+                        layers_dim={'h':[8, 32, 128], 
+                                    'g':[128, 64, 32, 16, 8]})#.float()
     else:
-        model = CNP_Net().float()
+        model = CNP_Net(io_dims=data_generator.io_dims)#.float()
 
-    learning_rate = args.learning_rate
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    max_iter = args.max_iter
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
     if args.fig_show:
+        fig = plt.figure()
         fig.show()
-    fig.canvas.draw()
+        ax = data_generator.make_fig_ax(fig)
+        fig.canvas.draw()
 
-    length_scale = 1 
-    noise = .1
-    kernel = RBF(length_scale=length_scale)+WhiteKernel(noise_level=noise**2)
-    gp = GaussianProcessRegressor(kernel=kernel, optimizer=None)
-
-    if args.func_limit != 0:
-        xs = np.zeros((args.func_limit, n_observation))
-        ys = np.zeros((args.func_limit, n_observation))
-        for i in range(args.func_limit):
-            xs[i] = (x_max-x_min) * (np.random.rand(n_observation) - .5)
-            ys[i] = gp.sample_y(xs[i].reshape(-1, 1)).reshape(-1)
-    
-    batch_size = {'P': args.batch_size[0], 'N':args.batch_size[1]}
-
-    for t in range(max_iter):
+    for t in range(args.max_epoch):
+        # print(t)
         loss = 0
-        for p in range(batch_size['P']):
+        #x, y = data_generator.generate_batch()
+        for p in range(args.batch_size):
             # data
-            if args.func_limit == 0:
-                x = (x_max-x_min) * (np.random.rand(n_observation) - .5)
-                x = x.reshape(-1, 1)
-                y = gp.sample_y(x).reshape(-1, 1)
-            else:
-                i = np.random.randint(args.func_limit)
-                x = xs[i].reshape(-1, 1)
-                y = ys[i].reshape(-1, 1)
-            idx = np.argsort(x)
-           
-            loss_ = 0
-            for n in range(batch_size['N']):
-                permutation = np.random.permutation(n_observation)
-                N = np.random.randint(1, n_observation)
+            # x, y = data_generator.generate_sample()
+            train, test = data_generator.get_train_test_sample()
+            # print('train shape', train)
+            # print('test shape', test)
+            x_train, y_train = train
+            x_test, y_test = test
 
-                x_train = x[permutation[:N]]
-                y_train = y[permutation[:N]]
-
-                gp.fit(x_train, y_train)
-                y_mu, y_cov = gp.predict(x_plot, return_cov=True)
-        
-                training_set = torch.cat((torch.tensor(x_train), 
-                            torch.tensor(y_train)),
-                        dim=1).float()
-                test_set = torch.cat((torch.tensor(x).float(), 
-                            torch.tensor(y).float()),
-                        dim=1).float()
+            # print(x_train, y_train, x_test, y_test)
+            # print('shapes', x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+            # print(x_test.shape, y_test.shape, x_train.shape, y_train.shape)
             
-                phi, log_prob = model(training_set, test_set)
-        
-                loss_ += -torch.sum(log_prob)
-            loss += loss_ / batch_size['N']
-        loss = loss / batch_size['P']
+#            if self.num_samples == 0:
+#                N = np.random.randint(self.num_samples_min, self.num_samples_max)
+#            else:
+#                N = self.num_samples
+#            if args.datasource == 'gp1d':
+#                gp = data_generator.gp
+#                gp.fit(x_train, y_train)
+#                #y_mu, y_cov = gp.predict(x_plot, return_cov=True)
+#                y_mu, y_cov = gp.predict(space_samples, return_cov=True)
+#            elif args.datasource == 'branin':
+#                pass
 
-        with open("logs/%s/log.txt"%args.log_folder, "a") as log_file:
-            log_file.write("%5d\t%10.4f\n"%(t, loss.item()))
+            # print('train', x_train.shape, y_train.shape)
+    
+            training_set = torch.cat((torch.tensor(x_train),
+                        torch.tensor(y_train)),
+                    dim=1).float()
+            test_set = torch.cat((torch.tensor(x_test).float(),
+                        torch.tensor(y_test).float()),
+                    dim=1).float()
         
-        if t % 1000 == 0:
+            # print('train, test', training_set.shape, test_set.shape)
+            phi, log_prob = model(training_set, test_set)
+            # print('phi', phi.shape)
+    
+            loss += -torch.sum(log_prob)
+        loss = loss / args.batch_size
+
+        if args.log:
+            with open("logs/%s/log.txt"%args.log_folder, "a") as log_file:
+                log_file.write("%5d\t%10.4f\n"%(t, loss.item()))
+        
+        if t % args.interval == 0:
             print('%5d'%t, '%10.4f'%loss.item())
-            plt.clf()
-            plt.xlim(x_min, x_max)
-            plt.ylim(-3, 3)
+            if args.fig_show:
+                plt.clf()
+                ax = data_generator.make_fig_ax(fig)
             
             # train, test points
-            plt.scatter(x, y, c='y')
-            plt.scatter(x_train, y_train, c='r')
+            # print(x_test.shape, y_test.shape)
+            # data_generator.plot_data(fig, np.concatenate((x_test, y_test), axis=1))
+            if args.fig_show:
+                data_generator.scatter_data(ax, np.concatenate((x_test, y_test), axis=1), c='y')
+                data_generator.scatter_data(ax, np.concatenate((x_train, y_train), axis=1), c='r')
 
-            # plot gp prediction (base line)
-            plot_fig(fig, x_plot, y_mu, y_cov)
-   
-            # plot model prediction
-            test_set = torch.cat((torch.tensor(x_plot),
-                    torch.tensor(np.zeros(n_observation).reshape(-1, 1))),
-                dim=1).float()
-            phi, _ = model(training_set, test_set)
-            predict_y_mu = phi[:,0].data.numpy()
-            predict_y_cov = np.diag(phi[:,1].data.numpy())**2
+    #                if x_test.shape[1] == 1:
+    #                    plt.scatter(x_test, y_test, c='y')
+    #                    plt.scatter(x_train, y_train, c='r')
+    #            
+    #                    # plot gp prediction (base line)
+    #                    plot_fig(fig, x_plot, y_mu, y_cov)
+       
+                # plot model prediction
+    #            print(x_plot.shape)
+    #            test_set = torch.cat((torch.tensor(x_plot),
+    #                    torch.tensor(np.zeros(len(x_plot)).reshape(-1, 1))),
+    #                dim=1).float()
+                # print(space_samples)
+                test_set = torch.cat((torch.tensor(space_samples),
+                        torch.tensor(np.zeros(len(space_samples)).reshape(-1, 1))),
+                    dim=1).float()
+                # print('train, test', training_set.shape, test_set.shape)
+                phi, _ = model(training_set, test_set)
+                # print('phi', phi.shape)
+                predict_y_mu = phi[:,:data_generator.io_dims[1]].data.numpy()
+                predict_y_cov = phi[:,data_generator.io_dims[1]:].data.numpy()**2
+    #            predict_y_mu_, predict_y_cov_, _ = model(training_set, test_set)
+    #            predict_y_mu = predict_y_mu_.data.numpy()
+    #            predict_y_cov = np.diag(predict_y_cov_.data.numpy())**2
+    
+                # plot_fig(fig, x_plot, predict_y_mu, predict_y_cov, color='b')
+                # print(space_samples.shape, predict_y_mu.shape, predict_y_cov.shape)
+                data_generator.plot_data(ax, np.concatenate((space_samples, predict_y_mu, predict_y_cov), axis=1))
+                fig.canvas.draw()
 
-            plot_fig(fig, x_plot, predict_y_mu, predict_y_cov, color='b')
-
-            fig.canvas.draw()
-
-            plt.savefig('logs/%s/%05d.png'%(args.log_folder, t))
-            torch.save(model, "logs/%s/%05d.pt"%(args.log_folder, t))
-         
+            if args.log:
+                plt.savefig('logs/%s/%05d.png'%(args.log_folder, t))
+                torch.save(model, "logs/%s/%05d.pt"%(args.log_folder, t))
+             
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
     
-    with open("logs/%s/log.txt"%args.log_folder, "a") as log_file:
-        log_file.write("%5d\t%10.4f\n"%(t, loss.item()))
+    if args.log:
+        with open("logs/%s/log.txt"%args.log_folder, "a") as log_file:
+            log_file.write("%5d\t%10.4f\n"%(t, loss.item()))
 
-    t = max_iter
+#    t = args.max_epoch
     print('%5d'%t, '%10.4f'%loss.item())
-    plt.clf()
-    plt.xlim(x_min, x_max)
-    plt.ylim(-3, 3)
+    if args.fig_show:
+        test_set = torch.cat((torch.tensor(space_samples),
+                torch.tensor(np.zeros(len(space_samples)).reshape(-1, 1))),
+            dim=1).float()
+        phi, _ = model(training_set, test_set)
+    
+        plt.clf()
+        ax = data_generator.make_fig_ax(fig)
+        data_generator.scatter_data(ax, np.concatenate((x_train, y_train), axis=1), c='r')
+        data_generator.scatter_data(ax, np.concatenate((x_test, y_test), axis=1), c='y')
+        data_generator.plot_data(ax, np.concatenate((space_samples, predict_y_mu, predict_y_cov), axis=1))
+        fig.canvas.draw()
+        fig.show()
+#    plt.clf()
+#    plt.xlim(x_min, x_max)
+#    plt.ylim(-3, 3)
 
     # train, test points
-    plt.scatter(x_train, y_train, c='r')
-    plt.scatter(x, y, c='y')
+#    plt.scatter(x_train, y_train, c='r')
+#    plt.scatter(x, y, c='y')
 
     # plot gp prediction (base line)
-    plot_fig(fig, x_plot, y_mu, y_cov)
+#    plot_fig(fig, x_plot, y_mu, y_cov)
 
     # plot model prediction
-    test_set = torch.cat((torch.tensor(x_plot),
-            torch.tensor(np.zeros(n_observation).reshape(-1, 1))),
-        dim=1).float()
-    phi, _ = model(training_set, test_set)
-    predict_y_mu = phi[:,0].data.numpy()
-    predict_y_cov = np.diag(phi[:,1].data.numpy())**2
+#    test_set = torch.cat((torch.tensor(x_plot),
+#            torch.tensor(np.zeros(n_observation).reshape(-1, 1))),
+#        dim=1).float()
+#    phi, _ = model(training_set, test_set)
+#    predict_y_mu = phi[:,0].data.numpy()
+#    predict_y_cov = np.diag(phi[:,1].data.numpy())**2
+#
+#    plot_fig(fig, x_plot, predict_y_mu, predict_y_cov, color='b')
 
-    plot_fig(fig, x_plot, predict_y_mu, predict_y_cov, color='b')
+#    fig.canvas.draw()
 
-    fig.canvas.draw()
-
-    plt.savefig('logs/%s/%05d.png'%(args.log_folder, t))
-    torch.save(model, "logs/%s/%05d.pt"%(args.log_folder, t))
+    if args.log:
+        torch.save(model, "logs/%s/%05d.pt"%(args.log_folder, t))
+        if args.fig_show:
+            plt.savefig('logs/%s/%05d.png'%(args.log_folder, t))
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,3 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--log", action='store_false')
+args = parser.parse_args()
+
+print(args.log)
+print(type(args.log))
+
+
+
+
+"""
 import numpy as np
 import torch
 from  torch.distributions.multivariate_normal import MultivariateNormal
@@ -8,5 +21,5 @@ cov = torch.tensor(4*(np.random.rand(51)-.5))**2
 
 normals = MultivariateNormal(mu, torch.diag(cov))
 print(normals)
-
+"""
 
