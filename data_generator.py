fb6f186a0b7379d424d54f26a87745630ba6f0ce
YoNyeoSeok <yys8646@postech.ac.kr>
2018-10-04 15:17:10 +0900

    before pull

--- data_generator.py
+++ data_generator.py
@@ -63,7 +63,13 @@ class DataGenerator():
 
         elif datasource == 'branin':
             def gen_param():
-                a, b, c, r, s, t = 1, 5.1/(4*np.pi**2), 5/np.pi, 6, 10, 1/(8*np.pi)
+                try:
+                    gen_param.first_call
+                    a, b, c, r, s, t = 1, 5.1/(4*np.pi**2), 5/np.pi, 6, 10, 1/(8*np.pi)
+                except AttributeError:
+                    gen_param.first_call = True
+                    a, b, c, r, s, t = 1, 5.1/(4*np.pi**2), 5/np.pi, 6, np.random.uniform(5, 15), 1/(8*np.pi)
+
                 return (a, b, c, r, s, t)
             self.gen_param = gen_param
 
